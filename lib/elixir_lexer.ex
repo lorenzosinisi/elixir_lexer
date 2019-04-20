a3e5e2f86a44a739aec959bca812a0f4f1fbce0a
lorenzosinisi <lasslo.net@gmail.com>
2019-04-20 16:01:55 +0200

    initial commit

---
+++ lib/elixir_lexer.ex
@@ -0,0 +1,18 @@
+defmodule ElixirLexer do
+  @moduledoc """
+  Documentation for ElixirLexer.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirLexer.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_lexer_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirLexerTest do
+  use ExUnit.Case
+  doctest ElixirLexer
+
+  test "greets the world" do
+    assert ElixirLexer.hello() == :world
+  end
+end
